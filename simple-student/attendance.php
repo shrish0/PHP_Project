d2f7cda1f45f3811bd08c0a3b09f032c6e136c0f
shrish0 <shrishgupta1234@gmail.com>
2024-07-25 15:56:26 +0530

    student management system minor ui update

--- simple-student/attendance.php
+++ simple-student/attendance.php
@@ -1,86 +1,117 @@
 <?php
-  include "cookie.php";
-  include "connectionString.php"
+include "connectionString.php";
+include "cookie.php";
+$sql = "SELECT * FROM attendance";
+$result = mysqli_query($con, $sql);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>attendance</title>
+  <title>Attendance Records</title>
   <link rel="stylesheet" href="css/stylead.css">
   <link rel="shortcut icon" href="img/logo.jpg" type="image/x-icon">
-</head>
-<body>
-    <div id="wrapper">
-        <div id="header">
-            <h1>attendance</h1>
-        </div>
-        <div id="menu">
-            <ul>
-                <li>
-                    <a href="option.html">back</a>
-                </li>
-                <li>
-                    <a href="adda.php">Add</a>
-                </li>
-            </ul>
-        </div>
-<style>
-#wrapper{
+  <style>
+  /* Move CSS styles into an external stylesheet */
+  #wrapper {
     width: 90%;
     margin: 50px auto 0;
     background-color: #fff;
-}
-</style>
-<div id="main-content">
-    <h2>All Records</h2>
-    <?php
-    
-     $sql="SELECT * FROM `attendance`;";
-     $result=mysqli_query($con,$sql) or die("query unsuccessful");
+    padding: 20px;
+  }
 
-     if(mysqli_num_rows($result)>0) {
-    ?>
-    <table cellpadding="7px">
-        <thead>
-        <th>student_id</th>
-        <th>student_name</th>
-        <th>attendance math</th>
-        <th>attendance ds</th>
-        <th>attendance dstl</th>
-        <th>attendance math</th>
-        <th>Action</th>
-        </thead>
-        <tbody>
-            <?php
-            while($row=mysqli_fetch_assoc($result))
-            {
-            ?>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+  }
+
+  table, th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+
+  @media only screen and (max-width: 600px) {
+    /* Adjustments for smaller screens */
+    #wrapper {
+      width: 100%;
+      margin: 20px auto 0;
+    }
+
+    table {
+      font-size: 14px;
+    }
+  }
+  </style>
+</head>
+<body>
+  <div id="wrapper">
+    <div id="header">
+      <h1>Attendance Records</h1>
+    </div>
+    <div id="menu">
+      <ul>
+        <li><a href="index.html">Back</a></li>
+        <li><a href="adda.php">Add</a></li>
+      </ul>
+    </div>
+
+    <div id="main-content">
+      <h2>All Records</h2>
+      
+      <?php if (mysqli_num_rows($result) > 0) { ?>
+        <table>
+          <thead>
             <tr>
-                <td><?php echo $row['student id']; ?></td>
-                <td><?php echo $row['student name']; ?></td>
+              <th>srno</th>
+              <th>Student Name</th>
+              <th>Attendance Math</th>
+              <th>Attendance DS</th>
+              <th>Attendance DSTL</th>
+              <th>Attendance COA</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php 
+            $counter = 1;
+            while ($row = mysqli_fetch_assoc($result)) { ?>
+              <tr>
+                <td><?php echo $counter; ?></td>
+                <td><?php echo $row['student_name']; ?></td>
                 <td><?php echo $row['attendance_math']; ?></td>
                 <td><?php echo $row['attendance_ds']; ?></td>
                 <td><?php echo $row['attendance_dstl']; ?></td>
                 <td><?php echo $row['attendance_coa']; ?></td>
                 <td>
-                    <a href='edita.php?id=<?php echo $row['student id'];?>'>Edit</a>
-                    <a href='delete.php?id=<?php echo $row['student id'];?>'>Delete</a>
+                  <a href='edita.php?id=<?php echo $row['student_id']; ?>'>Edit</a>
+                  <a href='delete.php?id=<?php echo $row['student_id']; ?>'>Delete</a>
                 </td>
-            </tr>
-            <?php } ?>
-           
-        </tbody>
-    </table>
-    <?php }
-    else{
-        echo "<h2>no record found<h2>";
-    } 
-    mysqli_close($con);
-    ?>
-</div>
-</div>
+              </tr>
+            <?php 
+            $counter++;
+            } ?>
+          </tbody>
+        </table>
+      <?php } else { ?>
+        <h3>No records found.</h3>
+      <?php } ?>
+
+    </div>
+  </div>
 </body>
 </html>
+
+<?php
+// Close database connection
+mysqli_close($con);
+?>
